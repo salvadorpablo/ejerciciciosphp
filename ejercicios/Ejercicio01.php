7fa9de23cffe6eb4c61d8d7426624c627206553d
salvadorpablo <salvadorpablol092@gmail.com>
2020-09-22 22:54:29 +0100

    ejercicio01

---
+++ ejercicios/Ejercicio01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ejercicio01</title>
+    </head>
+</head>
+<body>
+    <?php
+    $nombre = "Salvador";
+    $apellidos = "Pablo Latas";
+    ?>
+    
+    <table>
+        <tr>
+            <td><?php echo 'Nombre: '. $nombre. '<br>' ?></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    
+</body>
+</html>
