7fe0a175a2c342e9cdc4aa7ba078ce5a610fef4e
salvadorpablo <salvadorpablol092@gmail.com>
2020-09-28 16:14:37 +0100

    10.php

--- ejercicios/10.php
+++ ejercicios/10.php
@@ -9,8 +9,14 @@
         /* Repitite el ejercicio 6. En esta ocasión debes usar un array asociativo. 
         Cada posición se llamará correctamente: base, escolta, alero, 
         alapivot, pivot. Muestra el resultado con un "foreach (clave => valor)" */
+        $equipo = ["Base"=>"Carlos", "Escolta"=>"Alberto", "Alero"=>"Fran", "Ala-pívot"=>"Jorge", "Pívot"=>"Anton"];
 
-
+        echo 'Equipo de baloncesto con posiciones: <hr>';
+        echo "<ul>";
+        foreach($equipo as $posicion=>$element){
+            echo "<li>" . $posicion . ": " . $element . "</li>" ;
+        }
+        echo "</ul>"
         ?>
     </body>
 </html>
